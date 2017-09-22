b97cd86bac81ead5988242295a554d621ca2e352
Anthony <Anthony@BeastMode>
2017-09-21 20:31:03 -0400

    MiniProject4

--- MiniProject4/src/MiniProject4.java
+++ MiniProject4/src/MiniProject4.java
@@ -1,8 +1,10 @@
-
+/*Anthony Chaya
+ * Mini Project 4
+ * This program displays a rocket ship with the text Hello World in it*/
 public class MiniProject4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
