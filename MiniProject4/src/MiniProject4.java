0b7f2d3b8d36645f153727a6b510dc16ba5dbd43
Anthony <Anthony@BeastMode>
2017-09-21 20:42:36 -0400

    MiniProject4

--- MiniProject4/src/MiniProject4.java
+++ MiniProject4/src/MiniProject4.java
@@ -4,8 +4,29 @@
 public class MiniProject4 {
 
 	public static void main(String[] args) {
-		
-
+		topBottomRocket();
+		topBottomMid();
+		helloWorldMid();
+		topBottomMid();
+		topBottomRocket();
 	}
-
+	//This method displays the top of the rocket
+	public static void topBottomRocket() {
+		System.out.println("     /\\   ");
+		System.out.println("    /  \\  ");
+		System.out.println("   /    \\ ");
+	}
+	//This method displays the top mid and bottom mid boxes of the rocket
+	public static void topBottomMid() {
+		System.out.println("  +------+ ");
+		System.out.println("  |      | ");
+		System.out.println("  |      | ");
+		System.out.println("  +------+ ");
+	}
+	//This method displays Hello World in the middle box of the rocket
+	public static void helloWorldMid() {
+		System.out.println("  |Hello | ");
+		System.out.println("  | World| ");
+	}
+	
 }
