59b0132328f32eefae13fe89052d5322dab6e41d
Anthony <Anthony@BeastMode>
2017-09-21 20:28:37 -0400

    MiniProject3

--- MiniProject3/src/MiniProject3.java
+++ MiniProject3/src/MiniProject3.java
@@ -4,8 +4,20 @@
 public class MiniProject3 {
 
 	public static void main(String[] args) {
-		
+		topLine();
+		victoryWithBottom();
+		victoryWithBottom();
+		victoryWithBottom();
+		victoryWithBottom();
+		victoryWithBottom();
 
 	}
-
+	//This method prints out the top line of forward slashes
+	public static void topLine() {
+		System.out.println("//////////////////////");
+	}
+	//This method prints out the line Victory is mine! with the bottom forward slashes
+	public static void victoryWithBottom() {
+		System.out.println("|| Victory is mine! ||\n//////////////////////");
+	}
 }
