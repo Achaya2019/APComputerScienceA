98af9bbea2d494fe60123e48ab23572d9d46d66f
Anthony <Anthony@BeastMode>
2017-09-21 20:23:36 -0400

    MiniProject3

---
+++ MiniProject3/src/MiniProject3.java
@@ -0,0 +1,11 @@
+/*Anthony Chaya
+ * Mini Project 3
+ * This program displays the message "Victory is mine!" 5 times*/
+public class MiniProject3 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
