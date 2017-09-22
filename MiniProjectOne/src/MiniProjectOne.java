06a0c7196c7eaf433c191664a27d29b6bc21f156
achaya2019 <achaya2019@cchsraider.net>
2017-09-22 14:36:35 -0400

    MiniProject1

---
+++ MiniProjectOne/src/MiniProjectOne.java
@@ -0,0 +1,19 @@
+// Anthony Chaya
+//Displays Victory is Mine
+ 
+public class MiniProjectOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		victoryIsMine();
+
+	}
+	public static void victoryIsMine() {
+		// Prints out Victory is mine 
+		System.out.println("//////////////////////");
+		System.out.println("|| Victory is mine! ||");
+		System.out.println("//////////////////////");
+
+	}
+	// I PUSHED THIS TO GIT
+}
