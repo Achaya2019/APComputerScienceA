3fc022a1b79fbc3c69c1b5d061498fe424de55f5
Anthony <Anthony@BeastMode>
2017-09-24 20:37:06 -0400

    MiniProject7

---
+++ MiniProject7/src/MiniProject7.java
@@ -0,0 +1,19 @@
+/*Anthony Chaya
+ * Mini Project 7
+ * Displays text exacly as shown on paper*/
+public class MiniProject7 {
+
+	public static void main(String[] args) {
+		paragraph();
+
+	}
+	public static void paragraph() {
+		System.out.println("A \"quoted\" String is");
+		System.out.println("     'much' better if you learn");
+		System.out.println("the rules of \"escape sequences.\"");
+		System.out.println("     Also, \"\" represents an empty String");
+		System.out.println("Don't forget: use \\\" instead of \" !");
+		System.out.println("     '' is not the same as \"");
+	}
+
+}
