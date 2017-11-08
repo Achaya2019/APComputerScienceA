6b589d45ef967215921f8f83f778c81a42e8dd6d
Anthony <Anthony@BeastMode>
2017-11-08 18:31:05 -0500

    Random Problems

---
+++ MinutesInAYear/src/MinutesInAYear.java
@@ -0,0 +1,24 @@
+/* Anthony Chaya
+ * This program calculates and displays the amount of minutes there are in a year*/
+
+import java.util.Scanner;
+
+public class MinutesInAYear {
+
+	public static void main(String[] args) {
+		Scanner yearsEntered = new Scanner(System.in);
+		Double Years;
+		Double Months;
+		Double Days;
+		Double Hours;
+		Double Minutes;
+		
+		System.out.print("Enter amount of years to be converted to minutes:");
+		yearsEntered = yearsEntered.nextDouble();
+		
+		
+		
+	}
+
+	
+}
